8325b8144e5b6ad3e2ac86d5ba529cd958869274
Panoskontos <panos96el@gmail.com>
2022-06-09 11:34:17 +0300

    first commit

---
+++ WebApplication2/Controllers/HeroControllercs.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroControllercs : ControllerBase
+    {
+        //List of heroes
+        private static List<Hero> heroes = new List<Hero>
+            {
+                new Hero {
+                    Id=1,
+                    Name="Midorya",
+                    Description=". . . "
+                    ,Power="Strength"
+                },
+                 new Hero {
+                    Id=2,
+                    Name="Goku",
+                    Description="Is a sayan "
+                    ,Power="All"
+                },
+
+            };
+
+
+        //Get method all
+        [HttpGet]
+        public async Task<ActionResult<List<Hero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        // Get method one
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hero>> Get(int id)
+        {
+            var myhero = heroes.Find(h => h.Id == id);
+            if (myhero == null)
+            {
+                return BadRequest("Hero not found"); 
+            }
+            return Ok(myhero);
+        }
+
+        //Post method create
+        [HttpPost]
+        public async Task<ActionResult<List<Hero>>> AddHero(Hero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        
+    }
+}
---
+++ WebApplication2/Hero.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string Description { get; set; } = String.Empty;
+
+        public string Power { get; set; } = String.Empty;
+
+    }
+}
